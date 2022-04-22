89a6c8e6dacb989d3e4c27669e03a7b3bd2294d9
lu17 <glhcc001@163.com>
2022-04-22 15:32:49 +0800

    models: onnx: support yolox (#9)
    * onnx::yolox
    
    * onnx:yolox change lyngor detect
    
    * delete YOLOX and demo_output
    
    * update readme
    
    * update .patch
    
    * fix some bug
    
    * fix some bug 2
    
    * fix prepqre.sh install many times
    
    * update .patch
    
    * update patch
    
    * update new patch
    
    * add golden output
    
    Co-authored-by: lu gao <lu.gao@lynxi.com>

---
+++ models/onnx/vision/detection/yolox/1_prepare.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+echo ">准备环境，如失败请手动下载<"
+echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+
+source setting.cfg
+
+if [ ! -f "$model_file" ]; then
+    wget $model_url
+fi
+
+if [ ! -d "$source_dir" ]; then
+    git clone $source_url
+    cd $source_dir
+
+    # fixed torch 1.7.0 at first
+    pip3 install torch==1.7.0 torchvision
+    pip3 install -r requirements.txt
+fi
+
+cd $source_dir
+
+git checkout 6513f769fa500b3c7ad23b90a91dcbd8402be330
+
+# generate golden before patch
+export PYTHONPATH=$source_dir:$PYTHONPATH
+cd $golden_dir && python3 generate.py $model_file $input_shapes && cd -
+python3 $source_dir/demo/ONNXRuntime/onnx_inference.py -m $model_file -i $source_dir/assets/dog.jpg -s 0.3 --input_shape ${input_shapes##*"3,"} -o $golden_dir  
+
+# apply patch
+cd $source_dir
+git am ../patch/*.patch
+cd $cur_dir
---
+++ models/onnx/vision/detection/yolox/golden/generate.py
@@ -0,0 +1,20 @@
+import sys
+import torch
+import numpy as np
+import onnxruntime
+
+argc = len(sys.argv)
+if argc != 3:
+    print('usage: python3 verify.py <model_file> <input_shapes>')
+    assert(argc == 3)
+
+model_file = sys.argv[1]
+onnx_input_shape = tuple(map(int, sys.argv[2].split(",")))
+input = np.random.uniform(0.0, 1.0, onnx_input_shape).astype('float32')
+session = onnxruntime.InferenceSession(model_file)
+ort_inputs = {session.get_inputs()[0].name: input}
+inname = [input.name for input in session.get_inputs()]
+output = session.run(None, ort_inputs)
+
+input.tofile('input.bin')
+np.array(output).tofile('output.bin')
