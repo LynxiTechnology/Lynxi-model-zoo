6cd1a43abc76531ac1f5ae48accb7fcc119eccf9
lu gao <lu.gao@lynxi.com>
2022-06-13 18:36:53 +0800

    2_convert.sh update

--- models/darknet/vision/detection/yolov3/2_convert.sh
+++ models/darknet/vision/detection/yolov3/2_convert.sh
@@ -5,5 +5,4 @@ cd $cur_dir
 
 echo "======Compiling model by Lyngor======="
 
-# model_compile -f Keras_tf -m $model_file_h5 --input_shapes "$input_shapes" --input_nodes "input_1" --output "$output_dir"
 python lyncompile.py -m $model_file_h5 -o convert_out -i $input_shapes
