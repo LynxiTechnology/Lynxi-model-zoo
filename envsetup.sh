c9246eca6833fe74249d0f9f9b6832f1c67da517
Huangfei Xiao <huangfei.xiao@lynxi.com>
2022-06-14 13:56:42 +0800

    common: reorganize folders
    move lynpy to libs and others to tools

--- envsetup.sh
+++ envsetup.sh
@@ -4,6 +4,12 @@
 SHELL_FOLDER=$(pwd)
 
 export MODEL_ZOO_ROOT=$SHELL_FOLDER
-export MODEL_ZOO_TOOLS=$MODEL_ZOO_ROOT/common
+
+# for tools
+export MODEL_ZOO_TOOLS=$MODEL_ZOO_ROOT/common/tools
 export MODEL_VERIFY=$MODEL_ZOO_TOOLS/verify.py
 export PERF_TOOL=$MODEL_ZOO_TOOLS/lynperf.py
+
+# for libs
+export MODEL_ZOO_LIBS=$MODEL_ZOO_ROOT/common/libs
+export PYTHONPATH=$MODEL_ZOO_LIBS:$PYTHONPATH
