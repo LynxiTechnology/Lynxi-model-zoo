e518524afb0cf4ea2cfcf844c8fc958ed160609f
whzz101 <89677472+whzz101@users.noreply.github.com>
2022-10-21 17:21:05 +0800

    yolov5 add abc modify (#30)
    * add Real-ESRGAN lynxi support model
    
    * fix wget bug, modified detect output path
    
    * add Mobilenet_v2 caffe model
    
    * add Yolov5_deepsort model
    
    * add squeezenet_v1.1 caffe model
    
    * Create test.py
    
    modify print info
    
    * modify input output data
    
    * add abc modify
    
    * update yolov5 README.md
    
    Co-authored-by: zhou.zhou <zhou.zhou@lynxi.com>

--- models/pytorch/vision/detection/ultralytics-yolov5/2_convert.sh
+++ models/pytorch/vision/detection/ultralytics-yolov5/2_convert.sh
@@ -3,4 +3,13 @@
 source setting.cfg
 
 export PYTHONPATH=$source_dir:$PYTHONPATH
-model_compile -f Pytorch -m "$model_file" --input_shapes "$input_shapes" --input_nodes "data" --output_nodes $output_node --output "$output_dir"
+# model_compile -f Pytorch -m "$model_file" --input_shapes "$input_shapes" --input_nodes "data" --output_nodes $output_node --output "$output_dir"
+model_compile -f Pytorch -m "$model_file" \
+    --input_shapes "$input_shapes" \
+    --input_nodes "data" \
+    --output_nodes $output_node \
+    --output "$output_dir" \
+    --input_type "uint8" \
+    --output_type "float16" \
+    --normalization --mean 0,0,0 --variance 255,255,255 \
+    --transpose_axis NHWC    
